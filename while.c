b7890614f00b5523a277c57257584282e682c38c
OhYeMan <144315128+OhYeMan@users.noreply.github.com>
2023-09-07 14:55:50 +0700

    สูตรคูณ

--- while.c
+++ while.c
@@ -1,1 +1,12 @@
 #include <stdio.h>
+
+void main()
+{
+    int b = 0, i = 1;
+    printf("สูตรคูณ : ");
+    scanf("%d", &b);     
+    while( i<=12 ) {
+        printf("%d x %d = %d\n", b, i, (b*i));
+        i++;
+    }
+}
