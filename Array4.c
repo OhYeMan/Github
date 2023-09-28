bfb323c53eba207d768ebcc9e7a9e09220524479
OhYeMan <144315128+OhYeMan@users.noreply.github.com>
2023-09-28 17:24:31 +0700

    4

--- Array4.c
+++ Array4.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+char gradeChar(float gradeFloat);
 int main()
 {
     int i;
@@ -15,9 +16,33 @@ int main()
     }
     for (i = 0; i < 8; i++)
     {
-        printf("myGrade[%d] is %.1f\n",i+1,myGrade[i]);
+        gradeChar(myGrade[i]);
+        printf("myGrade[course %d] is %.1f (%c)\n",i+1,myGrade[i],gradeChar(myGrade[i]));
     }
-    
-return 0;
+ 
+    return 0;
+}
+char gradeChar(float gradeFloat)
+{
 
+    if (gradeFloat == 4.0)
+    {
+        return 'A';
+    }
+    else if (gradeFloat == 3.0 || gradeFloat == 3.5)
+    {
+        return 'B';
+    }
+    else if (gradeFloat == 2.0 || gradeFloat == 2.5)
+    {
+        return 'C';
+    }
+    else if (gradeFloat == 1.0 || gradeFloat == 1.5)
+    {
+        return 'D';
+    }
+    else if (gradeFloat == 0.0)
+    {
+        return 'F';
+    }
 }
